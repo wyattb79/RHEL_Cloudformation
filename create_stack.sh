38b3e168750c6d66c17dcc2ac5236d2cda398822
wyattb79 <nospam@fake.com>
2023-03-25 21:24:43 -0400

    Add stack creation script

---
+++ create_stack.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+source stack_config
+
+echo "Validating template"
+
+aws cloudformation validate-template --template-body file://cluster_template.yaml
+
+if [ "$?" -ne "0" ]; then
+  echo "Invalid template.  Exiting"
+  exit 1 
+fi
+
+echo "Do you wish to create this stack?"
+echo "Type YES in all uppercase to create it.  All other input exits"
+read create
+
+if [ "${create}" == "YES" ]; then
+  aws cloudformation create-stack --template-body file://cluster_template.yaml \
+    --stack-name ${cfn_stack_name} \
+    --parameters ParameterKey=MyIp,ParameterValue=${source_ip} \
+                 ParameterKey=VpcCidrBlock,ParameterValue=${vpc_cidr_block} \
+                 ParameterKey=SubnetCidrBlock,ParameterValue=${subnet_cidr} \
+                 ParameterKey=InstanceType,ParameterValue=${ex2_instance_type} 
+else
+  echo "Exiting"
+  exit 0   
+fi
